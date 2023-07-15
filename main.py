2c1432b0a43eceb6cfa83c16f80dcbac4b023629
Jesus Noland <jesus.noland@litpen.com>
2023-07-14 18:23:30 -0700

    Added listening software but still broken on Windows

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import chromadb
+from listener import main
 
 from chromadb.config import Settings
 client = chromadb.Client(Settings(
     chroma_db_impl="duckdb+parquet",
-    persist_directory="/path/to/persist/directory"
+    persist_directory="data"
 ))
+
+
+if __name__ == "__main__":
+    main(device="avfoundation", src=":1")
